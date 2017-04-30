00604c6d1db67cbf3a3cfb0d9172800a0d01f25f
adelegraziani <adele.graziani@studenti.unipd.it>
2017-04-30 15:16:43 +0200

    Update Prova.java
    modifiche parte due XD

--- src/src/Prova.java
+++ src/src/Prova.java
@@ -7,6 +7,8 @@ public class Prova {
         System.out.print("HEEEEELLLLLOOOOOO");
         System.out.print("HEEEEELLLLLOOOOOO");
         System.out.print("funziona can dal porco?");
+        System.out.print("Adele:ho fatto un fork della cartella e mi permette di modificare il file per poi uploaddare le modifiche");
+        System.out.print("provo l'upload");
     }
 
 }
